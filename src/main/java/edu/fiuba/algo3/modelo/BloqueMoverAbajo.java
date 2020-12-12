23065c285826c209c1e6af15432031664501b1ff
LHFPSystem <41213056+LHFPSystem@users.noreply.github.com>
2020-12-12 20:02:15 -0300

    added bloques mover personaje

---
+++ src/main/java/edu/fiuba/algo3/modelo/BloqueMoverAbajo.java
@@ -0,0 +1,11 @@
+package edu.fiuba.algo3.modelo;
+
+public class BloqueMoverAbajo implements Bloque{
+
+    @Override
+    public void EjecutarCompartamiento(Personaje unPersonaje){
+
+        System.out.println("Personaje Abajo");
+
+    }
+}
---
+++ src/main/java/edu/fiuba/algo3/modelo/BloqueMoverArriba.java
@@ -0,0 +1,11 @@
+package edu.fiuba.algo3.modelo;
+
+public class BloqueMoverArriba implements Bloque{
+
+    @Override
+    public void EjecutarCompartamiento(Personaje unPersonaje){
+
+        System.out.println("Personaje Arriba");
+
+    }
+}
