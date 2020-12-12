9e12cfb285783cb74b1a7dcf78b19ed9895807fd
LHFPSystem <41213056+LHFPSystem@users.noreply.github.com>
2020-12-12 19:44:49 -0300

    added personaje attributes and Lapiz class

--- src/main/java/edu/fiuba/algo3/modelo/Personaje.java
+++ src/main/java/edu/fiuba/algo3/modelo/Personaje.java
@@ -1,4 +1,9 @@
 package edu.fiuba.algo3.modelo;
 
 public class Personaje {
+
+    int posicionX;
+    int posicionY;
+    Lapiz unLapiz = new Lapiz();
+
 }
--- src/main/java/edu/fiuba/algo3/modelo/Tablero.java
+++ src/main/java/edu/fiuba/algo3/modelo/Tablero.java
@@ -7,5 +7,15 @@ import java.util.List;
 public class Tablero {
 
     List<Bloque> bloques = new ArrayList<Bloque>();
+    Personaje unPersonaje = new Personaje();
+
+    void ejecutarAlgoritmo(){
+
+        for (Bloque bloque : bloques) {
+
+            bloque.EjecutarCompartamiento(unPersonaje);
+
+        }
+    }
 
 }
