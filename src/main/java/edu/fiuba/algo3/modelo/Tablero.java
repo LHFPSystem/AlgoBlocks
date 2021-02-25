20b3c351ec3a5c9cf5442205cec0791ceea76f91
gcompanys <gonza.companis98@gmail.com>
2021-02-25 18:00:00 -0300

    metodo ejecutarAlgoritmo modificado

--- src/main/java/edu/fiuba/algo3/modelo/Tablero.java
+++ src/main/java/edu/fiuba/algo3/modelo/Tablero.java
@@ -21,6 +21,7 @@ public class Tablero {
 
         for (Bloque bloque : bloques) {
             bloque.ejecutar(unPersonaje);
+            this.dibujo.agregarTramo(unPersonaje);
         }
     }
 }
