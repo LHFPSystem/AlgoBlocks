b8798815fc65adba38698e09cecc7f42db0c1067
Giancarlo <gpuquio@fi.ub.ar>
2021-02-27 19:17:35 -0300

    Se agrego el boton abajo en el sector bloques

--- src/main/java/edu/fiuba/algo3/vista/ContenedorPrincipal.java
+++ src/main/java/edu/fiuba/algo3/vista/ContenedorPrincipal.java
@@ -1,5 +1,6 @@
 package edu.fiuba.algo3.vista;
 
+import edu.fiuba.algo3.vista.eventos.*;
 import edu.fiuba.algo3.modelo.Tablero;
 import javafx.geometry.Insets;
 import javafx.scene.Node;
@@ -9,54 +10,72 @@ import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class ContenedorPrincipal extends BorderPane {
 
-    VistaSectorBloques vistaSectorBloques = new VistaSectorBloques();
-    VistaSectorAlgoritmo vistaSectorAlgoritmo = new VistaSectorAlgoritmo();
-    VistaSectorDibujo vistaSectorDibujo = new VistaSectorDibujo();
+    VBox vistaSectorAlgoritmo = new VBox();
+    VBox vistaSectorDibujo = new VBox();
 
 
     public ContenedorPrincipal(Stage stage, Tablero tablero) {
 
-
-        this.setVistaSectorBloques();
+        this.setVistaSectorBloques(tablero, vistaSectorAlgoritmo);
         this.setVistaSectorAlgoritmo();
         this.setVistaSectorDibujo();
 
     }
 
 
-    private void setVistaSectorBloques(){
+    private void setVistaSectorBloques(Tablero tablero, VBox vistaSectorAlgoritmo){
+
+        List<String> bloques = new ArrayList<>();
 
         Label nombreVista = new Label("Bloques");
 
-        Button botonBloqueArriba = new Button();
-        botonBloqueArriba.setText("Arriba");
+        Button botonBloqueArriba = new Button("Arriba");
+        BotonBloqueArribaHandler botonBloqueArribaHandler = new BotonBloqueArribaHandler(tablero, vistaSectorAlgoritmo);
+        botonBloqueArriba.setOnAction(botonBloqueArribaHandler);
 
-        VBox sectorBloques = new VBox(new Node[]{nombreVista, botonBloqueArriba});
+        Button botonBloqueAbajo = new Button("Abajo");
+        BotonBloqueAbajoHandler botonBloqueAbajoHandler = new BotonBloqueAbajoHandler(tablero, vistaSectorAlgoritmo);
+        botonBloqueAbajo.setOnAction(botonBloqueAbajoHandler);
+
+
+        VBox sectorBloques = new VBox(new Node[]{nombreVista, botonBloqueArriba, botonBloqueAbajo});
         sectorBloques.setSpacing(10.0D);
         sectorBloques.setPadding(new Insets(15.0D));
-        //vistaBloques.setPrefWidth(400);
-        //vistaBloques.setPrefHeight(400);
+        sectorBloques.setPrefWidth(400);
+        sectorBloques.setPrefHeight(400);
         sectorBloques.setStyle("-fx-border-color: brown;");
         this.setLeft(sectorBloques);
-
-        //this.setLeft(this.vistaSectorBloques.getVista());
-
-        // crear boton moverArriba
 
     }
 
     private void setVistaSectorAlgoritmo(){
 
-        this.setCenter(this.vistaSectorAlgoritmo.getVista());
+        vistaSectorAlgoritmo.setPrefWidth(400);
+        vistaSectorAlgoritmo.setPrefHeight(400);
+        vistaSectorAlgoritmo.setStyle("-fx-border-color: brown;");
 
-        // etiqueta moverArriba
+        Label nombreVista = new Label("Algoritmo");
+        vistaSectorAlgoritmo.setMargin(nombreVista, new Insets(10, 10, 10, 10));
+        vistaSectorAlgoritmo.getChildren().add(nombreVista);
+        this.setCenter(vistaSectorAlgoritmo);
     }
 
     private void setVistaSectorDibujo(){
 
-        this.setRight(this.vistaSectorDibujo.getVista());
+        vistaSectorDibujo.setPrefWidth(400);
+        vistaSectorDibujo.setPrefHeight(400);
+        vistaSectorDibujo.setStyle("-fx-border-color: brown;");
+
+        Label nombreVista = new Label("Dibujo");
+        vistaSectorDibujo.setMargin(nombreVista, new Insets(10, 10, 10, 10));
+        vistaSectorDibujo.getChildren().add(nombreVista);
+        this.setRight(vistaSectorDibujo);
     }
 
 }
