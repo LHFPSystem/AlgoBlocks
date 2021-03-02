477ccbf524d01871bfe3c195564e038f92d02e4b
ferrero-manuel <maferrero@fi.uba.ar>
2021-03-02 00:39:04 -0300

    Se agregan las imagenes del personaje con el lapiz bajo y levantado y se visualizan en el contexto gráfico.

--- src/main/java/edu/fiuba/algo3/Controlador/BotonEjecutarHandler.java
+++ src/main/java/edu/fiuba/algo3/Controlador/BotonEjecutarHandler.java
@@ -5,9 +5,11 @@ import edu.fiuba.algo3.modelo.Tramo;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.canvas.Canvas;
+import javafx.scene.image.Image;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import edu.fiuba.algo3.modelo.Dibujo;
+
 
 public class BotonEjecutarHandler implements EventHandler<ActionEvent> {
     private Tablero tablero;
@@ -25,6 +27,10 @@ public class BotonEjecutarHandler implements EventHandler<ActionEvent> {
     public void handle(ActionEvent actionEvent) {
         this.tablero.ejecutarAlgoritmo();
         dibujo = this.tablero.getDibujo();
+        Image imagenPersonajeConLapizBajo =
+                new Image("file:src/main/java/edu/fiuba/algo3/vista/imagenes/personaje_con_lapiz_bajo.png");
+        Image imagenPersonajeConLapizLevantado =
+                new Image("file:src/main/java/edu/fiuba/algo3/vista/imagenes/personaje_con_lapiz_levantado.png");
 
         Tramo tramoInical = new Tramo(0,0,true);
         Tramo tramoFinal = dibujo.mostrarTramoYAvanzarAlSiguiente();
@@ -36,18 +42,24 @@ public class BotonEjecutarHandler implements EventHandler<ActionEvent> {
                 canvas.getGraphicsContext2D().strokeLine(tramoInical.obtenerPosicionX(), tramoInical.obtenerPosicionY(),
                         tramoFinal.obtenerPosicionX(), tramoFinal.obtenerPosicionY());
             }
-            tramoInical = tramoFinal;
-            tramoFinal = dibujo.mostrarTramoYAvanzarAlSiguiente();
-            if(tramoFinal != null){
-                tramoFinal = this.ajustarCoordenadas(tramoFinal);
+
+            if (tramoFinal.estaElLapizLevantado() == true) {
+                canvas.getGraphicsContext2D().drawImage(imagenPersonajeConLapizLevantado, tramoFinal.obtenerPosicionX(),
+                        tramoFinal.obtenerPosicionY(), 75, 75);
+            } else {
+                canvas.getGraphicsContext2D().drawImage(imagenPersonajeConLapizBajo, tramoFinal.obtenerPosicionX(),
+                        tramoFinal.obtenerPosicionY(), 75, 75);
             }
 
+            tramoInical = tramoFinal;
+            tramoFinal = dibujo.mostrarTramoYAvanzarAlSiguiente();
+            if(tramoFinal != null) {
+                tramoFinal = this.ajustarCoordenadas(tramoFinal);
+            }
         }
-
     }
 
     private Tramo ajustarCoordenadas(Tramo tramo){
-
         return new Tramo((tramo.obtenerPosicionX()*50)+100,-(tramo.obtenerPosicionY()*50)+100,tramo.estaElLapizLevantado());
     }
 
